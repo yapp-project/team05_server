b628c520f176ed987b87c24cc59acf361606a06b
wonheejeong <whj6648@gmail.com>
2019-04-05 14:15:09 +0900

    login,join with mysql

---
+++ db_info.js
@@ -0,0 +1,9 @@
+module.exports = {
+        local : {
+            host: 'localhost',
+            port : '3306',
+            user: 'root',
+            password: 'password',
+            database: 'test'
+        }
+    };
--- routes/join.js
+++ routes/join.js
@@ -1,68 +1,128 @@
-module.exports = function(app, Join)
+module.exports = function(app, connection, session)
 {
-	app.post('/api/join', function(req, res){
-    console.log('post /api/join');
-    var join = new Join();
-    join.userId = new String(req.body.userId),
-    join.userPw = new String(req.body.userPw),
-    join.userGen = new String(req.body.userGen),
-    join.userBirth = new String(req.body.userBirth),
-    join.userNick = new String(req.body.userNick),
-    join.userImg = new String(req.body.userImg),
-    join.interest = new String(req.body.interest),
-    join.gps_lat = new String(req.body.gps_lat),
-    join.gps_lan = new String(req.body.gps_lan)
-    join.save(function(err){
-        if(err){
-            console.error(err);
-            res.json({result: 0});
-            return;
+
+  //session login
+  app.get('/', (req, res) => {      // 1
+    console.log(req.session.logined);
+    if(req.session.logined) {
+      res.send({'code': 400});
+    } else {
+      res.send({'code':500});
+    }
+  });
+
+  //session logout
+  app.post('/logout', (req, res) => {      // 3
+    req.session.destroy();
+    // res.redirect('/');
+  });
+
+  //login
+  app.post('/login/login', function(req, res){
+    var userId = req.body.userId;
+    var userPw = req.body.userPw;
+    connection.query('SELECT * FROM jointable WHERE userId = ?', [userId],
+    function(error,results,fields){
+        if (error) {
+          res.json({
+            'status': 500
+          });
+        } else {
+            if(results.length > 0) {
+                if(results[0].userPw == userPw) {
+                  req.session.logined = true;
+                  req.session.userId = req.body.userId;
+                  res.json({
+                    'status': 200
+                  });
+                } else {
+                  res.json({
+                    'status': 300
+                  });
+                }
+            } else {
+              res.json({
+                'status': 400
+              });
+            }
         }
-
-        res.json({result: 1});
-
-    });
-     });
-
-     // GET ALL BOOKS
-app.get('/api/join', function(req,res){
-  console.log('get /api/join');
-    Join.find(function(err, join){
-        if(err) return res.status(500).send({error: 'database failure'});
-        res.json(join);
     })
-});
+  });
 
-	// app.get('/api/join', function(req, res){
-	// 	console.log('/api/join');
-	// 	join.find({}, function(err, join){
-	// 		if(err) return res.status(500).send({error: 'database failure'});
-	// 		res.json(join);
-	// 		res.end();
-	// 	});
-	// });
-	// app.delete('/api/join', function(req,res){
-  //               console.log('/api/join delete');
-  //               join.remove({userId: req.body[0].userId, userPw: req.body[0].userPw},function(err,join){
-  //                       if(err){
-  //                               console.log(error);
-  //                               return res.status(500).end('Database error');
-  //                       }
-  //
-  //                       res.end();
-  //               });
-  //       });
-  //
-	// function addjoin(userId, userPw){
-	// 	join.findOne({userId:userId, userPw:userPw}, function(err, con){
-	// 		if(!con){
-	// 			var join  = new join({userId:userId, userPw:userPw});
-	// 			join.save(function(err){
-	// 				if(err){
-	// 					console.error(err);
-	// 				}
-	// 			});
-	// 		}
-	// 	});
-	// }
+  //join
+   app.post('/login/join', function(req, res){
+    console.log('post /login/join');
+    var userId = req.body.userId;
+    var userPw = req.body.userPw;
+    var userGen = req.body.userGen;
+    var userBirth = req.body.userBirth;
+    var userNick = req.body.userNick;
+    var userImg = req.body.userImg;
+    var gps_lat = req.body.gps_lat;
+    var gps_lon = req.body.gps_lon;
+    var sql = 'INSERT INTO jointable SET ?;';
+    var params = {
+        "userId" : userId,
+        "userPw" : userPw,
+        "userGen" : userGen,
+        "userBirth" : userBirth,
+        "userNick" : userNick,
+        "userImg" : userImg,
+        "gps_lat" : gps_lat,
+        "gps_lon" : gps_lon
+    };
+    connection.query(sql,params, function (error, result,fields){
+        if(error) {
+            res.json({
+              'status': 400
+              // 'err :' + error
+            });
+            console.error('error', error);
+        }
+        else{
+            console.log(userId + ',' + userPw + ',' +gps_lon);
+            res.json({
+              'status': 200
+            });
+        }
+    });
+  });
+
+  // get all user data
+ app.get('/login/join', function(req,res){
+   console.log('get /login/join');
+   var statement = 'select * from jointable';
+        connection.query(statement, function (err, rows, fields){
+            if(err) return res.status(4000).send({error: 'database failure'});
+            else{
+                console.log('success ',rows);
+                res.send(rows);
+            }
+        });
+
+      });
+
+// app.get('/login/join', function(req,res){
+//     console.log('get /login/join');
+//     var statement = 'select * from jointable';
+//          connection.query(statement, function (err, rows, fields){
+//              if(err) return res.status(500).send({error: 'database failure'});
+//              else{
+//                  console.log('success ', rows);
+//                  res.send(rows);
+//              }
+//          });
+//
+//  });
+//    app.delete('/login/join', function(req,res){
+//                 console.log('/login/join delete');
+//                 statement = 'delete from jointable where userId = "' + req.body.userId +'";';
+//                 connection.query(statement, function (err, rows, fields){
+//                     if(err) return res.status(4000).send({error: 'database failure'});
+//                     else {
+//                         console.log('delete success');
+//                   }
+//                });
+//             });
+
 }
