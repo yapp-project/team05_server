1c725bc484aaa925afbdb3821f4d4d6e092fef54
daeun <wjdekdms99@naver.com>
2019-05-17 00:23:44 +0900

    modify attendant.js for adding maxOccupancyAlarm function

--- routes/meet/attendant.js
+++ routes/meet/attendant.js
@@ -1,12 +1,13 @@
 module.exports = function(app,connection)
 {
 //모임정보 상세 내 참여 신청
-    app.post('/meet/attendant', function(req,res){
+    app.get('/meet/attendant', function(req,res){
         console.log("post /meet/attendant");
-        var meetId = req.body.meet_Id;
+        var meetId = req.query.meet_Id;
         var meetCaptain;
         var attendantId = req.session.userId; 
-        var sql_one = "select m.fk_meetcaptain from meettable As m where m.meet_Id = " + meetId + ";";
+        var sql_one = "select fk_meetcaptain, meet_personNumMax from meettable where meet_Id = " + meetId + ";";
+        var sqltwo= "select count(*)+1 AS count from meetAttendants where fk_meet_Id = " + meetId + ";";
         var sql = 'INSERT INTO meetAttendants SET ?;';
         var params = {
             "fk_meet_Id" : meetId,
@@ -17,20 +18,22 @@ module.exports = function(app,connection)
                 res.status(400).send('err :' + error);
             else{
                 meetCaptain = result[0].fk_meetcaptain;
-                if(meetCaptain != attendantId){
-                    connection.query(sql,params,function(error, result, fields){
-                        if(error) res.status(400).json({"states" : 400});
-                        else{
-                            res.status(200).json({"states" : 200, "meet_Id" : meetId, "userId" : attendantId});
-                            console.log(meetId+' '+ attendantId);
-                        }
-                    });
-                }
-                else{
-                    res.status(300).json({"states" : 300, "sentence" : "the meetCaptain is already an attendant." });
-                    console.log("the meetCaptain is already an attendant.");
-                    res.end();
-                }
+                personNumMax = result[0].meet_personNumMax;
+                connection.query(sqltwo, function(err,row,fields){
+                    if(meetCaptain != attendantId && row[0].count < personNumMax){
+                        connection.query(sql,params,function(error, result, fields){
+                            if(error) res.status(400).json({"states" : 400});
+                            else{
+                                res.status(200).json({"states" : 200, "meet_Id" : meetId, "userId" : attendantId});
+                                console.log(meetId+' '+ attendantId);
+                            }
+                        });
+                    }
+                    else if(row[0].count == personNumMax){
+                        var alarm = require('../pushAlarm/maxOccupancyAlarm.js');
+                        alarm(meetId);
+                    }
+                });
             }
         });
   
