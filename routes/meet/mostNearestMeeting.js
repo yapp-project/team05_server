2cbaa83a53c714b86707d2df73aaa470bf56644c
daeun <wjdekdms99@naver.com>
2019-05-18 09:54:53 +0900

    modify mostNearestMeeting.js for edit post

--- routes/meet/mostNearestMeeting.js
+++ routes/meet/mostNearestMeeting.js
@@ -2,18 +2,19 @@
 module.exports = function(app,connection){
     var computeDistance = require('../module/computeDistance.js');
     
-    app.get('/meet/meetId/near',function(req,res){
+    app.post('/meet/meetId/near',function(req,res){
         console.log('get /meet/meetId/near');
         var meetId = new Array();
         var computeDistance = require('../module/computeDistance.js');
-        var longitude = req.query.myLongitude;
-        var latitude = req.query.myLatitude;
-        var myId = req.query.userId;
+        var longitude = req.body.myLongitude;
+        var latitude = req.body.myLatitude;
+        var myId = req.body.userId;
+        console.log(longitude,latitude,myId);
         var distance = new Array(); 
         var count = 0;
         var sqlone = "select meet_latitude, meet_longitude,meet_Id from meettable where meet_scheduledEnd = 0;";
-        var sqltwo = "select meet_name, meet_datetime, meet_Id, fk_meetcaptain, meet_personNumMax,"+
-        "meet_location from meettable where meet_scheduledEnd = 0 and ";
+        var sqltwo = "select meet_name, meet_datetime, meet_Id, fk_meetcaptain, meet_personNum,"+
+        "meet_location from meettable where meet_scheduledEnd = 0 and fk_meetcaptain != '" + myId + "' and(";
         connection.query(sqlone,function(error,result,fields){
             if(error){
                 res.status(400).json({"states" : 400});
@@ -33,11 +34,11 @@ module.exports = function(app,connection){
                                         current = Math.floor(Math.random() * Object.keys(result).length);
                                         console.log("변화하는 값 "+current);
                                 }
-                    
                     var destlat = result[current].meet_latitude;
                     var destlong = result[current].meet_longitude;
                     meetId[count] = result[current].meet_Id;
                     distance[count] = computeDistance(latitude,longitude,destlat,destlong);
+                    console.log(distance[count]);
                     if(distance[count] <= 1.0 ){
                         if(count == 0)
                             sqltwo = sqltwo.concat(" meet_Id = " + meetId[count]);
@@ -45,14 +46,14 @@ module.exports = function(app,connection){
                                 if(count < 4)
                                 	sqltwo = sqltwo.concat(" or meet_Id = " + meetId[count]);
                                 else
-                                    sqltwo = sqltwo.concat(" or meet_Id = " + meetId[count] + " ;");
+                                    sqltwo = sqltwo.concat(" or meet_Id = " + meetId[count] + " );");
                         }
                         console.log(past + " 현재 : " + current + " count " + count);
                         console.log(sqltwo);
                         count = count + 1;  
                 }
-                if(count == 5) break;
-                    }
+                    if(count == 5) break;
+                }
                 if(count == 0) res.status(300).json({"state" : 300, "string" : "there is no closest meeting."});
                 else {
                     connection.query(sqltwo,function(error,results,fields){
@@ -61,7 +62,7 @@ module.exports = function(app,connection){
                         console.log(error);
                     }
                     else{
-                        console.log(count);
+                        console.log(results);
                         for(var i = 0 ; i < count; i++){
                             results[i].distance = distance[i].toFixed(1);
                         }
