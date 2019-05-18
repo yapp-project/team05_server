249ab5b421a51dd93f49a9339fcedbf501e0ec0e
daeun <wjdekdms99@naver.com>
2019-05-18 11:37:33 +0900

    error

--- routes/pushAlarm/cancelAlarm.js
+++ routes/pushAlarm/cancelAlarm.js
@@ -3,7 +3,7 @@ module.exports = function(app,connection){
         var meetcaptain = req.query.userId;
         var meetId = req.query.meetId;
         var cancel;
-        var sqlone = "select a.fk_attendants_Id AS attendant, c.client_token AS clientToken " +
+        var sqlone = "select a.fk_attendants_Id AS attendant, c.usertoken AS clientToken " +
                  "from meetAttendants AS a" + " JOIN usertokens AS c ON c.fk_userId = a.fk_attendants_Id where a.fk_meet_Id = " +
                   meetId+" ;";
         var sqltwo = "select cancelReason from cancelreasons where fk_meetId = " + meetId+" ;";
