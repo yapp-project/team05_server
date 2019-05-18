67cf7818ff7dbdc7df185d30800b26c6311b722d
daeun <wjdekdms99@naver.com>
2019-05-18 10:25:35 +0900

    modify error

--- routes/meet/applyAttendant.js
+++ routes/meet/applyAttendant.js
@@ -6,7 +6,7 @@ module.exports = function(app,connection)
         var meetId = req.body.meetId;
         var meetCaptain;
         var attendantId = req.body.userId; 
-        var sql_one = "select fk_meetcaptain, meet_personNumMax from meettable where meet_Id = " + meetId + ";";
+        var sql_one = "select fk_meetcaptain, meet_personNum from meettable where meet_Id = " + meetId + ";";
         var sqltwo= "select count(*)+1 AS count from meetAttendants where fk_meet_Id = " + meetId + ";";
         var sql = 'INSERT INTO meetAttendants SET ?;';
         var params = {
@@ -19,9 +19,9 @@ module.exports = function(app,connection)
             else{
                 console.log(result);
                 meetCaptain = result[0].fk_meetcaptain;
-                personNumMax = result[0].meet_personNumMax;
+                personNum = result[0].meet_personNum;
                 connection.query(sqltwo, function(err,row,fields){
-                    if(meetCaptain != attendantId && row[0].count < personNumMax){
+                    if(meetCaptain != attendantId && row[0].count < personNum){
                         connection.query(sql,params,function(error, result, fields){
                             if(error) res.status(400).json({"state" : 400,"err":error});
                             else{
@@ -30,7 +30,7 @@ module.exports = function(app,connection)
                             }
                         });
                     }
-                    else if(row[0].count == personNumMax){
+                    else if(row[0].count == personNum){
                         var alarm = require('../pushAlarm/maxOccupancyAlarm.js');
                         alarm(meetId,res,connection);
                     }
--- routes/meet/markEndingSchedule.js
+++ routes/meet/markEndingSchedule.js
@@ -4,7 +4,7 @@ module.exports = function(app,connection){
         var meetId = req.body.meetId;
         var sql = "UPDATE meettable AS m SET m.meet_scheduledEnd = 1 WHERE m.meet_Id = " +meetId+";";
         connection.query(sql, function(error,result, fields){
-            if(error) res.status(400).json({"state" : 400});
+            if(error) res.status(400).json({"state" : 400,"err":error});
             else{
                 var endingMeetingAlarm = require('../pushAlarm/endingMeetingAlarm.js');
                 endingMeetingAlarm(connection,meetId,res);
