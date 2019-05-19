f8de0435bf70d45ea9b9b4d8323595c29f04ffc2
daeun <wjdekdms99@naver.com>
2019-05-20 00:50:28 +0900

    postDetail error

--- routes/meet/postDetail.js
+++ routes/meet/postDetail.js
@@ -42,7 +42,7 @@ module.exports = function(app,connection)
                         console.error('error'+ error);
                     }
                     else{
-                        var keywordCount = require('../module/findKeyword.js');
+                        var keywordCount = require('../keywordModule/findKeyword.js');
                         var key = req.body.keyword;
                         keywordCount(key,connection,result.insertId);
                         connection.query(sqlthree, param, function(error, rows, fields){
