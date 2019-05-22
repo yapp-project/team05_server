96d4075f383996a01e9fa3be7822c0f2134503d7
daeun <wjdekdms99@naver.com>
2019-05-23 02:04:45 +0900

    modify searchKeyword.js

--- routes/search/searchKeyword.js
+++ routes/search/searchKeyword.js
@@ -4,48 +4,44 @@ module.exports = function(app,connection){
         var keyword = req.query.keyword;
         var latitude = req.query.latitude;
         var longitude = req.query.longitude;
-        var meetPage = req.query.meetPage;
         var myId = null;
         var offset, firstIndex;
-        console.log(latitude+" "+longitude);
         var searching = new Object();
-        firstIndex = (parseInt(meetPage)-1) * 20;
         offset = 20;
         var sql = "select fk_meet_Id AS meetId, meet_keyword AS word from meetkeywords;";
         var idKeyArray = new Array();
 
-        connection.query(sql, function(error,result,fields){
+            connection.query(sql, function(error,result,fields){
             if(error){
                 res.status(400).json({"state" : 400});
                 console.log(error);
             }
-            else{
+            else{ 
                 var eqaulKeyword = require('../keywordModule/equalKeyword.js');
                 idKeyArray = eqaulKeyword(result,keyword);            
                     if(idKeyArray.length > 0){
                         var sqltwo = "select m.meet_Id as meet_Id,m.meet_name as meet_name, m.meet_datetime as meet_datetime, m.meet_location as meet_location"+
                         ", m.meet_personNum as meet_personNum, m.meet_latitude as meet_latitude, m.meet_longitude as meet_longitude" + 
-                        ",i.meetImg as meet_Img from meettable as m join meetimgs as i where ";
+                        ",i.meetImg as meet_Img from meettable as m join meetimgs as i on m.meet_Id = i.fkmeetId where meet_scheduledEnd = 0 and (";
                         for(var i = 0; i < idKeyArray.length; i++){
                             if(i != idKeyArray.length - 1){
-                                console.log(idKeyArray[i]);
                                 sqltwo = sqltwo.concat("m.meet_Id="+idKeyArray[i]+" or ");
-                                console.log(sqltwo);
                             }
                             else
-                                sqltwo = sqltwo.concat("m.meet_Id="+idKeyArray[i]+" limit " + firstIndex +", " + offset +";");
+                                sqltwo = sqltwo.concat("m.meet_Id="+idKeyArray[i]+");");
+                            if(i == offset - 1) break;
                         }
-
-
+                        console.log(sqltwo);
                             connection.query(sqltwo,function(error,results,fields){
-                                if(error) {res.status(400).json({"state": 400});
-                            console.log(error);
-                            }
+                                if(error) {
+                                    res.status(400).json({"state": 400});
+                                console.log(error);
+                                }
                                 else{
                                     if(Object.keys(results).length > 0){
-                                        console.log(results);
                                         var write = require('../sqlModule/writeSQLPtcNum.js');
                                         var sql = write(results);
+                                        console.log(sql);
                                         connection.query(sql,function(err,row,field){
                                             if(err){
                                                 res.status(400).json({"state":400});
@@ -73,15 +69,13 @@ module.exports = function(app,connection){
                                             var setPtcImage = require("../imageModule/setParticipantImage.js");
                                             var result = setPtcImage(results,row);
                                             var distanceSort = require('../sortModule/distanceSort.js');
-                                            var searchingResult = distanceSort(result,latitude,longitude);
+                                            var searchingResult = distanceSort(results,latitude,longitude);
                                             res.status(200).json({"state": 200, "list" : searchingResult});
                                         }
                                     });
-                                    
-                                    
                                 }
                             });
-                }
+                        }           
                 else{
                     var sqlthree = "select m.meet_Id,m.meet_name, m.meet_datetime, m.meet_location, m.meet_explanation, m.meet_personNum from meettable as m join meetviews as v on m.meet_Id = v.fk_meetId order by v.views asc ;"
                     connection.query(sqlthree,function(error,results,fields){
@@ -96,7 +90,6 @@ module.exports = function(app,connection){
                     // res.status(300).json({"states" : 300, "string" : "there is no meeting for the keyword."});
                 }
             }
-
         });
     }
 }
