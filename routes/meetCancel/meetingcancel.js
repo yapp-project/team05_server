71a3336b160b1199fabef238b274901682ead220
daeun <wjdekdms99@naver.com>
2019-05-24 18:07:35 +0900

    add meetingcancel.js

--- routes/meetCancel/meetingcancel.js
+++ routes/meetCancel/meetingcancel.js
@@ -1,8 +1,8 @@
 module.exports = function(app, connection){
     app.post('/meet/meetId',function(req,res){
         var meetId = req.body.meetId;
-        var sqlone = "DELETE from meetAttendants where fk_meet_Id = " + meetId + ";";
-        var sqltwo = "DELETE from meettable where meet_Id = " + meetId + ";";
+        var sqlone = "DELETE from meetAttendants where fk_meet_Id = " + meetId +";";
+        var sqltwo = "DELETE from meetendtable where meet_Id = " + meetId +";";
         connection.query(sqlone, function(error,rows,fields){
             if(error){
                 res.status(400).json({"state" : 400});
