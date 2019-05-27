681ab0690ff74e7cbd83dbde44edae65d37330ee
wonheejeong <whj6648@gmail.com>
2019-05-27 22:46:57 +0900

    modify getDetail.js

--- routes/meet/getDetail.js
+++ routes/meet/getDetail.js
@@ -7,11 +7,10 @@ module.exports = function(app,connection){
          "m.meet_explanation as meet_explanation,u.userNick as user_nick from meettable AS m join users as u on m.fk_meetcaptain = u.userId where m.meet_Id = " + meetId +";";
          var sqlone = "select m.fk_meetcaptain as captain_id, m.meet_latitude as latitude,m.meet_longitude as longitude, m.meet_personNum as person_num," +
          "m.meet_explanation as meet_explanation,u.userNick as user_nick from meetendtable AS m join users as u on m.fk_meetcaptain = u.userId where m.meet_Id = " + meetId +";";
-        
+
 
         var sqltwo = 'INSERT INTO meetviews(fk_meetId,views) VALUES(' + meetId + ',1) ON DUPLICATE KEY UPDATE fk_meetId='+meetId+', views=views+1;';
         console.log(sqltwo);
-        var sqltwo = 'IF EXISTS( SELECT fk_meetId FROM meetviews where fk_meetId='+meetId+') BEGIN UPDATE meetviews SET views = views+1 where fk_meetId ='+ meetId+'END ELSE BEGIN INSERT INTO meetviews(fk_meetId,views) VALUES('+meetId+',1) END';
         connection.query(sql, function(error,result,fields){
             if(error)
                 res.status(400).json({"state" : 400});
@@ -27,7 +26,7 @@ module.exports = function(app,connection){
                         result[0].endingflag = 0;
                         var getimgdetail = require("../detailModule/getimgdetail.js");
                         getimgdetail(connection,result,res,meetId);
-                       
+
                 }
                     else{
                         connection.query(sqlone, function(err,result,fields){
@@ -38,10 +37,10 @@ module.exports = function(app,connection){
                                     var getimgdetail = require("../detailModule/getimgdetail.js");
                                     getimgdetail(connection,result,res,meetId);
                                 }
-                            
-                            
+
+
                         });
-                        
+
                     }
                 }
                    //res.end();
