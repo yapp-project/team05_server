64b904bae1bd4bf6f152e6cde26430bc60b2e911
wonheejeong <whj6648@gmail.com>
2019-05-27 23:04:09 +0900

    modify mypage:

--- routes/mypage/mypage.js
+++ routes/mypage/mypage.js
@@ -28,9 +28,10 @@ module.exports = function(app, connection)
                         'state': 300
                       });
                     } else {
-                      connection.query('SELECT userImg from userimg where fk_userId = ?', userId,
+                      connection.query('SELECT userimg from userimg where fk_userId = ?', userId,
                       function(error4,results4,fields4){
                           if (error4) {
+                            console.log(error4);
                             res.json({
                               'state': 500
                             });
