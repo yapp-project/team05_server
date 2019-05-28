b573235cbc11688f8d3181eb2c26a8bef837bb57
wonheejeong <whj6648@gmail.com>
2019-05-29 01:27:11 +0900

    modify login

--- routes/login/join.js
+++ routes/login/join.js
@@ -134,25 +134,31 @@ module.exports = function(app, connection)
       var userId = req.body.userId;
       var userPw = req.body.userPw;
       connection.query('SELECT userPw FROM realusers WHERE userId = ?', [userId], function(error,results,fields){
-          console.log(results[0].userPw);
           if (error) {
             res.json({
               'state': 400
             });
           } else {
-            if (userPw == results[0].userPw) {
-              connection.query('DELETE FROM realusers WHERE userId = ?', [userId], function(error,results,fields){
-                if (error) {
-                  console.log(error);
-                  res.json({
-                    'state': 400
-                  });
-                } else {
-                  res.json({
-                    'state': 200
-                  });
-                }
-              })
+            if (results.length>0) {
+              if (userPw == results[0].userPw) {
+                connection.query('DELETE FROM realusers WHERE userId = ?', [userId], function(error,results,fields){
+                  if (error) {
+                    console.log(error);
+                    res.json({
+                      'state': 400
+                    });
+                  } else {
+                    res.json({
+                      'state': 200
+                    });
+                  }
+                })
+              }else {
+                res.json({
+                  'state': 300
+                });
+              }
+
             }else {
               res.json({
                 'state': 300
