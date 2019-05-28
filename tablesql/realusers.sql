0ac1e5bcc4fdac99958a9275507ed6369efa88bb
wonheejeong <whj6648@gmail.com>
2019-05-29 00:50:28 +0900

    add realuser.sql

---
+++ tablesql/realusers.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `realusers` (
+  `userId` varchar(20) NOT NULL,
+  `userPw` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
