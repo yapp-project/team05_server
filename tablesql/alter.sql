2b7f856224757774b2a10a1dd98b5606f4d1c8d0
wonheejeong <whj6648@gmail.com>
2019-05-29 00:55:39 +0900

    modify alter.sql

--- tablesql/alter.sql
+++ tablesql/alter.sql
@@ -8,13 +8,13 @@ alter table meetattendants drop foreign key `fk_attendants_Id`;
 alter table meetattendants add CONSTRAINT `fk_attendants_Id` FOREIGN KEY (`fk_attendants_Id`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 alter table userImg drop foreign key `fkuserId` ;
-alter table userImg add CONSTRAINT `fkuserId` FOREIGN KEY (`fk_userId`) REFERENCES `users` (`userId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+alter table userImg add CONSTRAINT `fkuserId` FOREIGN KEY (`fk_userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 alter table meetendtable drop foreign key `userIdid` ;
-alter table meetendtable add CONSTRAINT `userIdid` FOREIGN KEY (`fk_meetcaptain`) REFERENCES `users` (`userId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+alter table meetendtable add CONSTRAINT `userIdid` FOREIGN KEY (`fk_meetcaptain`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 alter table endmeetAttendants drop foreign key `fkattendantsId`;
-alter table endmeetAttendants add CONSTRAINT `fkattendantsId` FOREIGN KEY (`fk_attendants_Id`) REFERENCES `users` (`userId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+alter table endmeetAttendants add CONSTRAINT `fkattendantsId` FOREIGN KEY (`fk_attendants_Id`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 alter table noticecomment drop foreign key `fk_userId123` ;
-alter table noticecomment add CONSTRAINT `fk_userId123` FOREIGN KEY (`fk_userId`) REFERENCES `users` (`userId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+alter table noticecomment add CONSTRAINT `fk_userId123` FOREIGN KEY (`fk_userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
