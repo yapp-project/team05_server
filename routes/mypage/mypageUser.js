b7f907d3ff509d5172b2550b2c81b51d2c1fe5e4
wonheejeong <whj6648@gmail.com>
2019-05-30 07:05:41 +0900

    modify meetUser.js

--- routes/mypage/mypageUser.js
+++ routes/mypage/mypageUser.js
@@ -4,45 +4,47 @@ module.exports = function(app, connection)
   app.get('/mypage/user', (req, res) => {
     console.log("get /mypage/user");
     var userId = req.query.userId;
-    connection.query('SELECT * FROM users WHERE userId = ?',userId,
-    function(error,results,fields){
+    connection.query('SELECT * FROM users WHERE userId = ?',userId, function(error,results,fields){
         if (error) {
           console.log(error);
           res.json({
             'state': 400
           });
         } else {
-          connection.query('SELECT * FROM interests WHERE fk_userId = ?', userId,
-          function(error2,results2,fields2){
+          if (results.length > 0) {
+
+            connection.query('SELECT * FROM interests WHERE fk_userId = ?', userId, function(error2,results2,fields2){
               if (error2) {
                 console.log(error2);
                 res.json({
                   'state': 400
                 });
               } else {
-                connection.query('SELECT userImg from userImg where fk_userId = ?', userId,
-                function(error4,results4,fields4){
-                    if (error4) {
-                      console.log(error4);
-                      res.json({
-                        'state': 400
-                      });
-                    } else {
-                      res.send({
-                        'state': 200,
-                        'userNick' : results[0].userNick,
-                        'userGen' : results[0].userGen,
-                        'userBirth' :results[0].userBirth,
-                        'userImg' : results4[0],
-                        'interest':results2[0]});
+                connection.query('SELECT userImg from userImg where fk_userId = ?', userId, function(error4,results4,fields4){
+                  if (error4) {
+                    console.log(error4);
+                    res.json({
+                      'state': 400
+                    });
+                  } else {
+                    res.send({
+                      'state': 200,
+                      'userNick' : results[0].userNick,
+                      'userGen' : results[0].userGen,
+                      'userBirth' :results[0].userBirth,
+                      'userImg' : results4[0],
+                      'interest':results2[0]});
 
                     }
-                })
+                  })
 
-              }
-          })
+                }
+              })
 
-        }
+            }else {
+              res.send({'state':400});
+            }
+          }
     })
 
 
