3ee429e286c75c3436ea8604a599565ece43f198
daeun <wjdekdms99@naver.com>
2019-05-29 22:15:31 +0900

    fit plan

--- routes/imageModule/setParticipantImage.js
+++ routes/imageModule/setParticipantImage.js
@@ -2,21 +2,15 @@ module.exports = function(results,row){
     if(Object.keys(row).length > 0){
         for(var i = 0; i < Object.keys(results).length; i++){
             var participantImgArray = new Array();
-            participantImgArray.push("https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg");
             for(var j = 0; j < Object.keys(row).length; j++){
-                
-                if(results[i].meet_Id == row[j].meet_Id&&participantImgArray[0] == "https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg"){
-                    participantImgArray.pop();
-                    if(row[j].userImg == "null") row[j].userImg = "https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg";
-                    participantImgArray.push(row[j].userImg);
-                }
-                else if(results[i].meet_Id == row[j].meet_Id&&participantImgArray.length < 4){
+                if(results[i].meet_Id == row[j].meet_Id&&participantImgArray.length < 4){
                     if(row[j].userImg == "null") row[j].userImg = "https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg";
                     participantImgArray.push(row[j].userImg);
                 }
                 else if(participantImgArray.length == 4)
                 break;
             }
+            if(participantImgArray.length == 0) participantImgArray.push("https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg");
             results[i].participantImg = participantImgArray;
         }   
     }
