4d9ebb341965f0d1d5fd0d8d78fd1874d552fc9e
daeun <wjdekdms99@naver.com>
2019-05-29 22:05:44 +0900

    null image -> basic image

--- routes/imageModule/setParticipantImage.js
+++ routes/imageModule/setParticipantImage.js
@@ -2,10 +2,10 @@ module.exports = function(results,row){
     if(Object.keys(row).length > 0){
         for(var i = 0; i < Object.keys(results).length; i++){
             var participantImgArray = new Array();
-            participantImgArray.push("null");
+            participantImgArray.push("https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg");
             for(var j = 0; j < Object.keys(row).length; j++){
                 
-                if(results[i].meet_Id == row[j].meet_Id&&participantImgArray[0] == "null"){
+                if(results[i].meet_Id == row[j].meet_Id&&participantImgArray[0] == "https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg"){
                     participantImgArray.pop();
                     participantImgArray.push(row[j].userImg);
                 }
@@ -20,7 +20,7 @@ module.exports = function(results,row){
     }
     else{
         for(var i = 0; i < Object.keys(results).length; i++)
-            results[i].participantImg = "null";
+            results[i].participantImg = "https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg";
     }
 
     return results;
