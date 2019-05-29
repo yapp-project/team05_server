8806c2548906bc4dc722462eadc3016720a43881
daeun <wjdekdms99@naver.com>
2019-05-29 22:10:38 +0900

    modify setParticipantImage

--- routes/imageModule/setParticipantImage.js
+++ routes/imageModule/setParticipantImage.js
@@ -7,9 +7,11 @@ module.exports = function(results,row){
                 
                 if(results[i].meet_Id == row[j].meet_Id&&participantImgArray[0] == "https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg"){
                     participantImgArray.pop();
+                    if(row[j].userImg == "null") row[j].userImg = "https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg";
                     participantImgArray.push(row[j].userImg);
                 }
                 else if(results[i].meet_Id == row[j].meet_Id&&participantImgArray.length < 4){
+                    if(row[j].userImg == "null") row[j].userImg = "https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg";
                     participantImgArray.push(row[j].userImg);
                 }
                 else if(participantImgArray.length == 4)
