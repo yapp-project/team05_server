e2b0b64cb15df1705556f77489c252c57f455977
daeun <wjdekdms99@naver.com>
2019-05-31 15:13:53 +0900

    delete randommeetimg.js

--- tablesql/randommeetimg.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `randommeetimgs` (
-  `randomId` int(10) unsigned NOT NULL,
-  `meetImg` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`randomId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
