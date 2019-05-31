756fbcbefa8b481b532aa7c5370a08bfa55b5cc3
daeun <wjdekdms99@naver.com>
2019-06-01 02:32:11 +0900

    add participants id in meet detail

--- routes/detailModule/getimgdetail.js
+++ routes/detailModule/getimgdetail.js
@@ -1,21 +1,21 @@
 module.exports = function(connection,result,res,meetId){
     var sqltwo,sqlthree,sqlfour;
     if(result[0].flag == 0){
-        sqltwo = "select u.userImg as participants_img from meetAttendants as m join userImg as u on m.fk_attendants_Id = u.fk_userId where " +
+        sqltwo = "select u.userImg as participants_img, m.fk_attendants_Id as participants_Id from meetAttendants as m join userImg as u on m.fk_attendants_Id = u.fk_userId where " +
         "m.fk_meet_Id = " + meetId +  ";";
         sqlthree = "select u.userImg as captain_img from meettable as m join userImg as u on m.fk_meetcaptain = u.fk_userId where " +
         "m.meet_Id = " + meetId + ";";
         sqlfour = "select count(*)+1 as participants_num from meetAttendants where fk_meet_Id = " + meetId + ";";
     }
     else{
-        sqltwo = "select u.userImg as participants_img from endmeetAttendants as m join userImg as u on m.fk_attendants_Id = u.fk_userId where " +
+        sqltwo = "select u.userImg as participants_img, m.fk_attendants_Id as participants_Id from endmeetAttendants as m join userImg as u on m.fk_attendants_Id = u.fk_userId where " +
         "m.fk_meet_Id = " + meetId +  ";";
         sqlthree = "select u.userImg as captain_img from meetendtable as m join userImg as u on m.fk_meetcaptain = u.fk_userId where " +
         "m.meet_Id = " + meetId + ";";
         sqlfour = "select count(*)+1 as participants_num from endmeetAttendants where fk_meet_Id = " + meetId + ";";
     }
     var imgArray = new Array();
-    
+    var idArray = new Array();
     connection.query(sqltwo, function(err,row,field){
         if(err) {res.status(400).json({"state" : 400}); console.log(err)}
         else{
@@ -23,15 +23,13 @@ module.exports = function(connection,result,res,meetId){
                 for(var i = 0; i < Object.keys(row).length; i++){
                     if(row[i].participants_img=="null") imgArray.push("https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg");
                     else imgArray.push(row[i].participants_img);
+                    idArray.push(row[i].participants_Id);
                 }
                 
                 result[0].participants_img = imgArray;
+                result[0].participants_Id = idArray;
             }
             
-            else{
-                imgArray.push("https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg");
-                result[0].participants_img = imgArray;
-            }
                 
 
             connection.query(sqlthree,function(err,row,field){
@@ -42,8 +40,6 @@ module.exports = function(connection,result,res,meetId){
                         else 
                         result[0].captain_img = row[0].captain_img;
                     }
-                    else
-                        result[0].captain_img = "https://s3-us-west-2.amazonaws.com/yappsimmo/meetimg/1.jpg";
                     connection.query(sqlfour,function(err,row,field){
                         if(err)res.status(400).json({"state" : 400, "err" : err});
                         else{
